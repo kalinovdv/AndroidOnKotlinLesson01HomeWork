537c8e76271dc6bee7172e9e6b18918043918f16
kalinovdv <kdv_ul@rambler.ru>
2021-08-10 12:40:54 +0400

    Перевел класс NoteData на Kotkin.

---
+++ app/src/main/java/ru/geekbrains/mymenuauth/data/NoteData.kt
@@ -0,0 +1,49 @@
+package ru.geekbrains.mymenuauth.data;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Date;
+
+class NoteData : Parcelable{
+    var id : String
+    var titel : String
+    var text : String
+    var date : Date
+
+    constructor(titel : String, text : String, date : Date) {
+        this.titel = titel
+        this.text = text
+        this.date = date
+        this.id = ""
+    }
+
+    constructor(parsel : Parcel) {
+        titel = parsel.readString()?:""
+        text = parsel.readString()?:""
+        date = Date(parsel.readLong())
+        id = parsel.readString()?:""
+    }
+
+    companion object CREATER : Parcelable.Creator<NoteData> {
+        override fun createFromParcel(parsel : Parcel) : NoteData {
+            return NoteData(parsel);
+        }
+
+        override fun newArray(size : Int) : Array<NoteData?> {
+            return arrayOfNulls(size);
+        }
+    };
+
+    override fun describeContents() : Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest : Parcel, flags : Int) : Unit {
+        dest.writeString(titel)
+        dest.writeString(text)
+        dest.writeString(id)
+        dest.writeLong(date.getTime())
+    }
+
+}
