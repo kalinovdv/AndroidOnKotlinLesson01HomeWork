53466ee2ac3521d447a9db0e396da80a70b2c641
kalinovdv <kdv_ul@rambler.ru>
2021-08-10 19:43:48 +0400

    Перевел интерфейс NoteSource на Kotkin.

---
+++ app/src/main/java/ru/geekbrains/mymenuauth/data/NotesSource.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.mymenuauth.data;
+
+interface NotesSource {
+    fun init(notesSourceResponse : NotesSourceResponse) : NotesSource
+    fun getNoteData(position : Int) : NoteData
+    fun size() : Int
+    fun addNoteData(noteData : NoteData) : Unit
+    fun deleteNoteData(position : Int) : Unit
+    fun updateNoteData(position : Int, noteData : NoteData) : Unit
+    fun clearNoteData() : Unit
+}
